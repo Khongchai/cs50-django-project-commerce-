a6d34998f55d711bfba5bfb13b64812d90c06735
khongchai <world1955@hotmail.com>
2020-08-18 23:32:19 +0700

    added html listing page and all models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "listingItemName", "price", "listingDate")
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listedProducts")
+    listingItemName = models.CharField(max_length=64)
+    price = models.IntegerField()
+    listingDate = models.DateTimeField(auto_now_add=True, blank=True)
+    listingDescription = models.TextField(default="Enter description")
+
+    def __str__(self):
+        return f"ID: {self.id}\n Product: {self.listingItemName}\n Posted by: {self.owner} \n Price: {self.price} \n Listing date: {self.listingDate}"
+
+class Bid(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="biddedProducts")
+    biddedItemName = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    bidDate = models.DateTimeField(auto_now_add=True, blank=True)
+
+class Comment(models.Model):
+    content = models.TextField(default="Enter comment")
+    commentedOn = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    #do not delete comments when users are deleted.
+    commentor = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="commentor", null=True, blank=True)
+    commentedDate = models.DateTimeField(auto_now_add=True, blank=True)
+
+
